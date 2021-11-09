e87792a6a14200b4c4fadd1de3ba6aef885fcfd8
Joni3187 <oni.rene@outlook.com>
2021-11-09 01:40:42 -0600

    push

--- config/plugins.js
+++ config/plugins.js
@@ -5,6 +5,6 @@ module.exports = ({ env }) => ({
             cloud_name: env("CLOUDINARY_NAME"),
             api_key: env("CLOUDINARY_KEY"),
             api_secret: env("CLOUDINARY_SECRET")
-        }
-    }
-})
+        },
+    },
+});
